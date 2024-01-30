75b2069c9bd8feb62bce93c48c66ad741a332e90
Jakub Gardon <kubag960@gmail.com>
2024-01-30 15:02:15 +0100

    nowy commit

--- nowy z kamilem.py
+++ nowy z kamilem.py
@@ -1,1 +1,24 @@
 #funkcje
+
+def przywitaj():
+    print('.................')
+    print('Witamy na spotkaniu')
+    print('plik z logami uaktualniony')
+
+
+def przywitaj_spersonalizowane1(x):
+    print('Siema ',x)
+    print(f'Siema {x}')
+
+while True:
+    decision = input('Czy przywitać? T/N ')
+    if decision.lower() == 't':
+        przywitaj()
+    else:
+        print('Brak przywitania')
+        break
+
+imie = input('Podaj imie: ')
+przywitaj_spersonalizowane1(imie)
+print('................')
+przywitaj_spersonalizowane1('Erwina')
